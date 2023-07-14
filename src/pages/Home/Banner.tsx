d3041f13f94090202f25dbea5b1a8dd84127176a
zillurahman10 <zillur.hero40@gmail.com>
2023-07-14 11:44:14 +0600

    banner added

--- src/pages/Home/Banner.tsx
+++ src/pages/Home/Banner.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import bannerPhoto from "../../assets/Back to school books colorful cartoon brown yellow Instagram Post.png";
 
 const Banner = () => {
-    return (
-        <div>
-            Banner
+  return (
+    <div>
+      <div className="hero min-h-screen">
+        <div className="hero-content flex flex-col lg:flex-row justify-between">
+          <img
+            src={bannerPhoto}
+            className="max-w-sm rounded-lg shadow-2xl mr-24"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">Enlarge your knowladge</h1>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../Shared/Header";
 import Books from "./Books";
+import Banner from "./Banner";
 
 const Home = () => {
   return (
     <div>
       <Header></Header>
+      <Banner></Banner>
       <Books></Books>
     </div>
   );
