42df7ab70c28666613358814c375970f3e2df60e
zillurahman10 <zillur.hero40@gmail.com>
2023-07-16 22:57:39 +0600

    readme file added

---
+++ readme.md
@@ -0,0 +1,5 @@
+- Clint site live link: https://64b420a52922995257067a7a--book-catalog-zr.netlify.app/
+
+- Client site GitHub: https://github.com/zillurahman10/book-catalog
+
+- Sever site GitHub: https://github.com/zillurahman10/book-catalog-server
--- src/redux/api/apiSlice.ts
+++ src/redux/api/apiSlice.ts
@@ -2,7 +2,9 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const api = createApi({
   reducerPath: "api",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://book-catalog-server-kappa.vercel.app/",
+  }),
   tagTypes: ["reviews", "books"],
   endpoints: (builder) => ({
     getAllBooks: builder.query({
